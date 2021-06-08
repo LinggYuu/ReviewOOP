4c5cfc61b02bc6c2e4ef770a7a9408ce4f45e253
Linggg <lingyu4113@gmail.com>
2021-06-08 13:58:35 +0800

    git test

--- src/main/java/com/ly/service/classtest.java
+++ src/main/java/com/ly/service/classtest.java
@@ -1,4 +1,30 @@
 package com.ly.service;
 
 public class classtest {
+
+}
+
+
+
+//客户类
+class customer{
+    //属性
+    String name;
+    int age;
+    boolean isMale;
+    //方法
+    public void eat(){
+        System.out.println("客户吃饭");
+    }
+    public void sleep(int hour){
+        System.out.println("休息"+hour);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getNation(String nation){
+        String info = "我的国籍："+nation;
+        return info;
+    }
 }
